a94c29c7a327085fa408b732c460a25bb3fb9b77
Astral Gnome <reedkeenan@gmail.com>
2020-11-29 22:26:02 -0800

    adding persistent local storage to list

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,95 @@
 //WHEN the user clicks the search button
 
-function handleSearch(){
-    //THEN I get the value entered into the search input
+$("#search-button").on("click", setText);
 
-    makeWeatherRequest(search)
+var locationArray = []
+
+function setText(event) {
+  event.preventDefault();
+  
+  var textArea = $("#city");
+  
+  var textAreaValue = textArea.val();
+  
+  var storageKey = textArea.attr("id");
+  
+  localStorage.setItem(storageKey, JSON.stringify(textAreaValue));
+  
+  
+  
+  locationArray.unshift(textAreaValue)
+  
+  localStorage.setItem("cities", JSON.stringify(locationArray))
+  
+  var storedCities = JSON.parse(localStorage.getItem("cities")) || [] //not sure what this does on the end here
+  
+  console.log(storedCities)
+  
+  
+  for (var i = 0; i < storedCities.length; i++) {
+    
+    $("#li-"+i).text(storedCities[i])
+  }
+
+  for (var i = 0; i < locationArray.length; i++) {
+    
+    $("#li-"+i).text(locationArray[i])
+  }
 
 }
 
-function makeWeatherRequest(search) {
+function handleSearch() {
+    var storedCity = JSON.parse(localStorage.getItem("city"));
+    makeWeatherRequest(storedCity);
+    console.log(storedCity)
+  };
+
+function makeWeatherRequest(cityName) {
+  //NEXT, make the request to the URL with JQuery ajax
+      //NEXT, we need to build the URL for the first API request
+      
+      //NEXT, make the request to the URL with JQuery ajax
+      
+      // finish rendering data to HTML
+  
+      // var cityName = localStorage.getItem(storageKey)
+  var queryURL =
+  `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=2aea25a2ba73c351a3ca2212a808cc72`;
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // Printing the entire object to console
+    console.log(response.main.temp);
+    console.log(response);
+    
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
     
     //NEXT, make the request to the URL with JQuery ajax
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2aea25a2ba73c351a3ca2212a808cc72";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-
-      // Printing the entire object to console
-        console.log(response);
-
-
-    //NEXT, make the request to the URL with JQuery ajax
-    
-//     $.ajax(queryURL).then(function (response){
-        
-        //START rendering data to the HTML
-        
-        //THEN get the lat and lng out of the "response" object
-
-        //NEXT call "makeOneCallRequest(lat, lng)" and pass in the lat and lng
-
-
+    var forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=2aea25a2ba73c351a3ca2212a808cc72`
+    $.ajax(forecastURL).then(function (forecastResponse) {
+      console.log(forecastResponse)
+      
+      var uv = forecastResponse.current.uvi;
+      console.log(uv)
+      
+      //START rendering data to the HTML. How would I accomplish this with back-ticks?
+      
+      $("#main-city").text(response.name)
+      $("#main-temp").text("Temperature: " + response.main.temp + " °F")
+      $("#main-hum").text("Humidity: " + response.main.humidity + "%")
+      $("#main-wind").text("Wind speed: " + response.wind.speed + " mph")
+      $("#main-uv").text("UV index: " + uv)
+      
+      //THEN get the lat and lng out of the "response" object
+      //NEXT call "makeOneCallRequest(lat, lng)" and pass in the lat and lng
     });
+  }); 
 }
-// function makeOneCallRequest(lat, lng) {
 
-    //NEXT, we need to build the URL for the first API request
-
-    //NEXT, make the request to the URL with JQuery ajax
-//     $.ajax(queryURL).then(function (response){
-        
-        // finish rendering data to HTML
-    
-//     }); 
-// }
-
+// var cityName = $("#city-input").text()
+// console.log(cityName)
 //pseudo code the HTML
+handleSearch();
